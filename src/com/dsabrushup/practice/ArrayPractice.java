e2dcdcfdd29056e90c03f3b5c4589f11b656796f
Mohamed Abdul <mohamedyousufmo@gmail.com>
2022-02-20 20:15:44 -0800

    Array Sample

---
+++ src/com/dsabrushup/practice/ArrayPractice.java
@@ -0,0 +1,21 @@
+package com.dsabrushup.practice;
+
+public class ArrayPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// declaring array
+		int nums[] = new int[3];
+		
+		// first index always 0
+		nums[0] = 13;
+		nums[1] = 5;
+		nums[2] = 45;
+		nums[3] = 98;
+		
+		// invalid index
+		nums[4] = 9;
+	}
+
+}
